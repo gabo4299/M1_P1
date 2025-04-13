8d2fa05bb04356a8ad2b7c1d6ca136accb275ba7
Gabriel Bellido <gabrielcuellar99@hotmail.com>
2025-04-13 04:31:59 -0400

    configs

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,14 +6,26 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions:{
+      ssl:{
+        requiere:true,
+        rejectUnauthorized:false
+      }
+    }
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME + '_test',
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions:{
+      ssl:{
+        requiere:true,
+        rejectUnauthorized:false
+      }
+    }
   },
   production: {
     username: process.env.DB_USERNAME,
